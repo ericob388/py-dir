1e0a53008f230ba0fb2bf2ef7bfe2d0e6a616650
ericob388 <38667393+ericob388@users.noreply.github.com>
2018-04-27 00:41:27 +0200

    Update test_helpers.py

--- tests/test_helpers.py
+++ tests/test_helpers.py
@@ -19,4 +19,6 @@ class TestIOHelper(unittest.TestCase):
 
 
 if __name__ == '__main__':
-    unittest.main()
+    import xmlrunner
+    unittest.main(testRunner=xmlrunner.XMLTestRunner(output='test-reports'))
+ 
